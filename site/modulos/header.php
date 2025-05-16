5c6f65e2b471a959e42353059b8478fce6922c73
Andres <anondiaz@gmail.com>
2025-05-16 10:39:24 +0200

    Cambios formalizacion codificar

--- site/modulos/header.php
+++ site/modulos/header.php
@@ -2,23 +2,23 @@
 $verificarUsuario = isset($_SESSION['id_usuario']) && $_SESSION['id_usuario'];
 ?>
 <header>
-    <div class="header-arriba">
-        <div class="logo">
-            <h1>TaskFlow</h1>
-            <?= $verificarUsuario == '' ? '<img src="img/logo-largo.avif" alt="Logo TaskFlow">' : '<img src="../gestor_tareas_site/img/logo-largo.avif" alt="Logo TaskFlow">' ?>
-        </div>
-        <div class="login-gestion">
-                <?= $verificarUsuario == '' ? '<a href="entrada_usuarios.php">Login</a>' : '<a href="../gestor_tareas_site/logout.php">Logout</a>' ?>    
-                <?= $verificarUsuario == '' ? '' : '<a href="gestion-usuario.php">Gestión de cuenta</a>' ?> 
-        </div>
+    <div class="logo">
+        <h1>TaskFlow</h1>
+        <?= $verificarUsuario == '' ? '<img src="img/logo-largo.avif" alt="Logo TaskFlow">' : '<img src="../gestor_tareas_site/img/logo-largo.avif" alt="Logo TaskFlow">' ?>
     </div>
-    <?php if(!$verificarUsuario = isset($_SESSION['id_usuario']) && $_SESSION['id_usuario']) : ?>
     <nav>
-        <ul>
-            <li><a href="index.php#features">Características</a></li>
-            <li><a href="alta_usuarios.php">Create una Cuenta</a></li>
-            <li><a href="index.php#contact">Contacto</a></li>
-        </ul>
+        <?php if (!$verificarUsuario = isset($_SESSION['id_usuario']) && $_SESSION['id_usuario']) : ?>
+            <ul class="menu">
+                <li><a href="index.php#features">Características</a></li>
+                <li><a href="index.php#contact">Contacto</a></li>
+                <li><a href="alta_usuarios.php">Create una Cuenta</a></li>
+                <li><a href="entrada_usuarios.php">Login</a></li>
+            </ul>
+        <?php else : ?>
+            <ul class="login-gestion">
+                <li><a href="../gestor_tareas_site/logout.php">Logout</a></li>
+                <li><a href="gestion-usuario.php">Gestión de cuenta</a></li>
+            </ul>
+        <?php endif; ?>
     </nav>
-    <?php endif; ?>
 </header>
